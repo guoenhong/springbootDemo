8d9064e42e079fe01d066216e3679cc193c9b9d9
En Hong Guo <enhguo@cn.ibm.com>
2019-08-23 17:37:06 +0800

    update .yml

---
+++ src/main/java/com/example/demo/mapping/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.mapping;
+
+import com.example.demo.pojo.User;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface UserMapper {
+    @Select("select * from user_info where name = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Insert("insert into user_info(name,sex) values(#{name}, #{sex})")
+    int insertUser(@Param("name") String name,@Param("sex") String sex);
+}
